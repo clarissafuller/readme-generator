22c658c2b1ab651dd0fafd8fcdb183390107d4c1
clarissafuller <141360959+clarissafuller@users.noreply.github.com>
2023-10-04 17:24:46 -0400

    fixes checkbox option in prompts

--- Develop/index.js
+++ Develop/index.js
@@ -1,50 +1,55 @@
 // TODO: Include packages needed for this application
-const inquirer = require('inquirer');
-const fs = require('fs');
+const inquirer = require("inquirer");
+const fs = require("fs");
 // TODO: Create an array of questions for user input
 inquirer
   .prompt([
     {
-      type: 'input',
-      name: 'project title',
-      message: 'What is your project title?',
+      type: "input",
+      name: "project title",
+      message: "What is your project title?",
     },
     {
-      type: 'input',
-      name: 'project description',
-      message: 'Please provide a brief description of the project?',
+      type: "input",
+      name: "project description",
+      message: "Please provide a brief description of the project?",
     },
     {
-      type: 'input',
-      name: 'installation instructions',
-      message: 'What is your favorite hobby?',
+      type: "input",
+      name: "installation instructions",
+      message: "How do you install your project?",
     },
     {
-      type: 'input',
-      name: 'usage information',
-      message: 'What is your favorite food?',
+      type: "input",
+      name: "usage information",
+      message: "How is this project used?",
     },
     {
-      type: 'input',
-      name: 'contribution guidelines',
-      message: 'Enter your GitHub Username',
+      type: "input",
+      name: "contribution guidelines",
+      message: "Please enter any contributors to this project",
     },
     {
-      type: 'input',
-      name: 'test instructions',
-      message: 'Enter your LinkedIn URL.',
+      type: "input",
+      name: "test instructions",
+      message: "Please enter any test instructions for your project",
     },
     {
-        type: 'checkbox',
-        name: 'license',
-        message: 'Enter your LinkedIn URL',
-        choices: 'MIT', 'Microsoft Public License', 'Academic Free License', 'GPL',
-      },
+      type: "checkbox",
+      name: "license",
+      message: "Enter your LinkedIn URL",
+      choices: [
+        "MIT",
+        "GPL",
+        "Creative Commons license",
+        "Microsoft Public License",
+      ],
+    },
   ])
   .then((answers) => {
     const htmlPageContent = generateHTML(answers);
-    fs.writeFile('index.html', htmlPageContent, (err) =>
-      err ? console.log(err) : console.log('Successfully created index.html!')
+    fs.writeFile("index.html", htmlPageContent, (err) =>
+      err ? console.log(err) : console.log("Successfully created index.html!")
     );
   });
 
